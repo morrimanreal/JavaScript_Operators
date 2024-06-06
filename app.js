ad1b0ba63fb155070ee7858bc2ccc1c7b7f6243d
Morricone Manreal <morriconemanreal@gmail.com>
2024-06-06 15:04:28 -0400

    finished exercise

--- app.js
+++ app.js
@@ -8,12 +8,71 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let a = 20;
+let b = 4;
+
+let add = a+b;
+let minus = a-b;
+let multiply = a*b;
+let dividing = a/b;
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+let question1 = num + str;
+let question2 = num + str2;
+let question3 = num + isPresent;
+let question4 = firstName + num;
+let question5 = isPresent + str;
+let question6 = firstName + lastName;
+
+console.log(question1);
+console.log(question2);
+console.log(question3);
+console.log(question4);
+console.log(question5);
+console.log(question6);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+let ques1 = (val==str3);
+let ques2 = (val === str3);
+let ques3 = (!isPresent2);
+let ques4 = ("eleven" == str4 && val >= str3);
+let ques5 = (!isPresent2 || isPresent2);
+let ques6 = (0 == false);
+let ques7 = (0 === false);
+let ques8 = (0 != false);
+let ques9 = (0 !== false);
+
+console.log(ques1);
+console.log(ques2);
+console.log(ques3);
+console.log(ques4);
+console.log(ques5);
+console.log(ques6);
+console.log(ques7);
+console.log(ques8);
+console.log(ques9);
